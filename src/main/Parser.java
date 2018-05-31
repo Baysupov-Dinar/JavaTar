155f7c907d2cd42fbfbaeb69fb0391f39f4f12ce
Baysupov-Dinar <dinar.baysupov@gmail.com>
2018-06-01 01:38:52 +0300

    fixes

--- src/main/Parser.java
+++ src/main/Parser.java
@@ -1,33 +1,39 @@
 package main;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public static void main(String[] args) {
+    public void main(String[] args) throws Exception {
         List<File> fileList = new ArrayList<>();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].equals("-u")) {
-                fileList.add(new File(args[i + 1]));
-                new Tar(fileList).unWeld();
-                break;
+        try {
+            for (int i = 0; i < args.length; i++) {
+                if (args[i].equals("-u")) {
+                    fileList.add(new File(args[i + 1]));
+                    new Tar(fileList).unWeld();
+                    break;
+                }
+                if (args[i].equals("-out")) {
+                    if (fileList.isEmpty()) throw new IllegalArgumentException();
+                    String outName = args[i + 1];
+                    new Tar(fileList, outName).weld();
+                    break;
+                } else {
+                    File tmpFile = new File(args[i]);
+                    if (tmpFile.exists()) fileList.add(tmpFile);
+                    else {
+                        System.out.println("Some files does not exist");
+                        throw new IllegalArgumentException();
+                    }
+                }
             }
-            if (args[i].equals("-out")) {
-                if (fileList.isEmpty()) throw new IllegalArgumentException();
-                String outName=args[i+1];
-                new Tar(fileList, outName).weld();
-                break;
+            if (fileList.isEmpty()) {
+                System.out.println("Error, illegal arguments");
             }
-            else{
-                File tmpFile=new File(args[i]);
-                if(tmpFile.exists())fileList.add(tmpFile);
-                else throw new IllegalArgumentException("Some files does not exist");
-            }
-
-        }
-        if (fileList.isEmpty()) {
-            System.out.println("Error, illegal arguments");
+        } catch (IOException ioEx) {
+            throw Tar.getEx();
         }
     }
 }
--- src/main/Tar.java
+++ src/main/Tar.java
@@ -4,20 +4,20 @@ import java.io.*;
 import java.util.List;
 
 public class Tar {
+    private static Exception ex;
     private List<File> fileList;
     private String outFile = "";
     private String key = "#!++";
-
-    public Tar(List<File> fileList) {
+    Tar(List<File> fileList) {
         this.fileList = fileList;
     }
 
-    public Tar(List<File> fileList, String outName) {
+    Tar(List<File> fileList, String outName) {
         this.fileList = fileList;
         this.outFile = outName;
     }
 
-    public void weld() {
+    public void weld() throws Exception {
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(outFile))) {
             Boolean check=false;
             while(!check){
@@ -37,12 +37,10 @@ public class Tar {
                 br.close();
             }
             bw.write(key);
-        } catch (IOException ex) {
-            throw new IllegalArgumentException("Can't reach files");
-        }
+        }throw ex;
     }
 
-    public void unWeld() {
+    public void unWeld() throws Exception {
         try (BufferedReader br = new BufferedReader(new FileReader(fileList.get(0)))){
             String curr;
             String finalKey=br.readLine();
@@ -54,11 +52,14 @@ public class Tar {
                 }
                 bw.close();
             }
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-        }
+        }throw ex;
     }
-    public boolean checkKey()throws IOException{
+
+    public static Exception getEx(){
+        return ex;
+    }
+
+    private boolean checkKey()throws IOException{
         for (File elem : fileList){
             BufferedReader br = new BufferedReader(new FileReader(elem));
             String curr;
@@ -68,7 +69,6 @@ public class Tar {
                     return false;
                 }
             }
-
         }
         return true;
     }
